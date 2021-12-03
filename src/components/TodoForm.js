bd84ee0cc48c487011e3afc86d6fa718642cd036
Buna Cezar <bunacezar@Bunas-MacBook-Pro.local>
2021-12-03 16:47:44 +0200

    commit2

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function TodoForm() {
+    const[input,setInput] = useState('');
     return (
         <div>
-            
+            <form className="todo-form">
+                <input 
+                type='text'
+                placeholder ='Add a todo'
+                value={input}
+                name='text'
+                className='todo-input'
+                />
+                <button className='todo-button'>Add todo</button>
+            </form>
         </div>
     )
 }
